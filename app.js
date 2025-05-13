cda93ab7da81161b600b88f98588a95ec36c34e8
Pranit Shrivastava <pranitshrivastava010@gmail.com>
2025-05-13 14:43:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,4 +37,7 @@ app.use("/", indexRouter);
 app.use("/blog", blogRouter);
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
